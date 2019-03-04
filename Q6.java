83c25e83d180c0bc4a617a0a5a61571352046e73
RiteshSingh0097 <ritesh893@outlook.com>
2019-03-04 11:50:03 +0530

    java code

--- Q6.java
+++ Q6.java
@@ -4,36 +4,35 @@
 package com.bootcamp.collection;
 
 import java.util.*;
-import java.lang.*;
 
 public class Q6 {
 
 
-    public static Map<Integer,Integer> sortByValue(Map<Integer,Integer> map){
+    public static Map<Integer, Integer> sortByValue(Map<Integer, Integer> map) {
 
         List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
 
 
-        Collections.sort(list,(Map.Entry<Integer,Integer> o1, Map.Entry<Integer,Integer> o2)-> {
+        Collections.sort(list, (Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) -> {
             return -o1.getValue().compareTo(o2.getValue());
         });
-
+    
         Map<Integer, Integer> temp = new LinkedHashMap<>();
 
-        for(Map.Entry<Integer, Integer> aa : list){
-            temp.put(aa.getKey(),aa.getValue());
+        for (Map.Entry<Integer, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
         }
-        return map;
+        return temp;
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int[] arr = new int[size];
         int occurences = 1;
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new LinkedHashMap<>();
 
-        for (int i=0;i<arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
 
             if (map.containsKey(arr[i])) {
@@ -42,10 +41,10 @@ public class Q6 {
                 map.put(arr[i], occurences);
         }
 
-        Map<Integer,Integer> map1 = sortByValue(map);
+        Map<Integer, Integer> map1 = sortByValue(map);
 
-        for (Map.Entry<Integer,Integer> en : map1.entrySet()){
-            System.out.println("Element = "+en.getKey()+" frequency = "+en.getValue());
+        for (Map.Entry<Integer, Integer> en : map1.entrySet()) {
+            System.out.println("Element = " + en.getKey() + " frequency = " + en.getValue());
         }
     }
 }
